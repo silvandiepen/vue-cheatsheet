5269cda2338f02763ab07cf823e6d72fbc37bdb2
Sil van Diepen <me@silvandiepen.com>
2019-01-15 10:24:25 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,114 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <section class="container">
+        <div class="content">
+          <h1>Cheatsheet</h1>
+        </div>
+      </section>
+      
+      <div class="container content">
+          <h2>Javascript</h2>      
+      </div>
+      
+      <section class="background--yellow">
+        <div class="container content">
+          <row>
+            <column medium="1:2">
+              <vue-markdown>
+#### Custom Properties
+
+Add or read custom properties
+              </vue-markdown>
+            </column>
+            <column medium="1:2">
+             <vue-markdown>
+**Set**
+```js
+element.style.setProperty(propertyName, value, priority);
+````
+
+**Get**
+```js
+element.style.getProperty(propertyName);
+```
+**Remove**
+```js
+element.style.removeProperty(propertyName);
+```   
+         
+          </vue-markdown>
+            </column>
+          </row>
+        </div>
+      </section>
+      
+      
+      
+      <div class="container content">
+          <h2>Misc.</h2>      
+      </div>
+      
+      
+        <section class="background--green">
+        <div class="container content">
+          <row>
+            <column medium="1:2">
+              <vue-markdown>
+#### To Lowercase
+
+There are so many ways to make a string lowercase, every language has its own function. 
+              </vue-markdown>
+            </column>
+            <column medium="1:2">
+             <vue-markdown>
+**SCSS**
+```scss
+to-lower-case($string);
+````
+
+**JS**
+```js
+toLowerCase(string);
+```
+**PHP**
+```php
+strtolower(string);
+```   
+         
+          </vue-markdown>
+            </column>
+          </row>
+        </div>
+      </section>
+      
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import VueMarkdown from "vue-markdown";
+import Grid from "@sil/grid";
+
+const Row = Grid.row;
+const Column = Grid.column;
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    VueMarkdown,
+    Column,
+    Row
   }
 };
 </script>
 
+
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import "~@sil/base-style/src/scss/index.full";
+
+code[class*="language"] {
+  background-color: color(Black, 0.85);
+  border: 1px solid color(Black, 1);
+  color: color(White);
+  padding: 1rem;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
+import "highlight.js/styles/default.css";
+import Highlight from "vue-markdown-highlight";
+
+Vue.use(Highlight);
 
 Vue.config.productionTip = false;
 
